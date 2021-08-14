01453984ab624d03aeec221590cc767f9e1cb256
Zac Waite <129032699zw@gmail.com>
2021-08-14 18:27:50 -0400

    Branch route completed, without backend connection

--- backend/src/route/task.route.js
+++ backend/src/route/task.route.js
@@ -1,7 +1,7 @@
 const express = require("express"); //Used for server
 const router = express.Router(); //Allows for rerouting different requests 
-const taskCtrl = require("../api/task"); //Takes user input and sends to DAO
+const taskCtrl = require("../oldFiles/task"); //Takes user input and sends to DAO
 
 router.route("/").get(taskCtrl.apiGetTask); //Send request to GET task on default
 
-module.exports = router; //Exports class for routing information about restaurants
+module.exports = router; //Exports class for routing information about tasks
