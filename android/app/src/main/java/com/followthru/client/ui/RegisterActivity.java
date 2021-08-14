89b26872d8a3a950a30b445d946d0c8b7e2d16a9
KevDev0247 <donkevin1980s@gmail.com>
2021-08-14 19:11:30 -0400

    Register page UI logic

--- android/app/src/main/java/com/followthru/client/ui/RegisterActivity.java
+++ android/app/src/main/java/com/followthru/client/ui/RegisterActivity.java
@@ -2,7 +2,10 @@ package com.followthru.client.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.followthru.client.R;
 
@@ -12,5 +15,25 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        TextView usernameInput = findViewById(R.id.username_input);
+        TextView passwordInput = findViewById(R.id.password_input);
+
+        TextView loginEntry = findViewById(R.id.login_entry);
+        loginEntry.setOnClickListener(v -> {
+            Intent intent = new Intent();
+            intent.setClass(RegisterActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+
+        Button registerButton = findViewById(R.id.login_button);
+        registerButton.setOnClickListener(v -> {
+            String username = usernameInput.getText().toString();
+            String password = passwordInput.getText().toString();
+
+            Intent intent = new Intent();
+            intent.setClass(RegisterActivity.this, ProjectActivity.class);
+            startActivity(intent);
+        });
     }
 }
